35a7d59f35ffba13aa70ca448ca6c8d159c4f670
Vann Teves <vteves@platform9.com>
2017-03-24 14:39:24 -0700

    Use a blank cors config for development
    Right now this changeset would make this the default.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 var app = express();
- 
+
+//TODO(vann): Set this configuration correctly when in production
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
 var routes = require("./routes/routes.js")(app);
- 
-var server = app.listen(80, function () {
+
+var server = app.listen(9000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
